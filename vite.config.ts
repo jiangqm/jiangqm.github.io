0e266902114336dab5f5760b64bd057ab20222e5
jiangqm <1107816226@qq.com>
2023-05-29 16:55:00 +0800

    update: 更新部署路径

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'website',
+  base:'/blob/feature/website',
   build:{
     outDir:'docs'
   },
